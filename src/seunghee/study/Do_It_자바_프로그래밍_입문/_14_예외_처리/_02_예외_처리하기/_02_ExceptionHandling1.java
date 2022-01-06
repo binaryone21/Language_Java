5338eacc53a2e0a4ed273d7aab4b3e2af76ac514
seunghee <binaryone21@gmail.com>
2022-01-07 00:52:10 +0900

    Java

---
+++ src/seunghee/study/Do_It_자바_프로그래밍_입문/_14_예외_처리/_02_예외_처리하기/_02_ExceptionHandling1.java
@@ -0,0 +1,15 @@
+package seunghee.study.Do_It_자바_프로그래밍_입문._14_예외_처리._02_예외_처리하기;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class _02_ExceptionHandling1 {
+    public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("_02_text.txt");
+        } catch(FileNotFoundException e) {
+            System.out.println(e);
+        }
+        System.out.println("여기도 수행됩니다.");
+    }
+}
