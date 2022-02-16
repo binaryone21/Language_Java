179f7ef3cbb355faef8a422382889b7b81e98e33
binaryone21 <binaryone21@gmail.com>
2022-02-16 19:26:54 +0900

    Java

---
+++ src/seunghee/_Do_it_자바_완전_정복/_01_자바_시작하기/_04_자바_프로그램의_기본_구조/_01_println.java
@@ -0,0 +1,13 @@
+package seunghee._Do_it_자바_완전_정복._01_자바_시작하기._04_자바_프로그램의_기본_구조;
+
+public class _01_println {
+    public static void main(String[] args) {
+        System.out.println("화면 출력");
+        System.out.println("화면" + "출력");
+        System.out.println(3.8);
+        System.out.println(3 + 5);
+        System.out.println("화면" + 3);
+        System.out.println("화면" + 3 + 5);
+        System.out.println(3 + 5 + "화면");
+    }
+}
---
+++ src/seunghee/_Do_it_자바_완전_정복/_01_자바_시작하기/_04_자바_프로그램의_기본_구조/_02_variable.java
@@ -0,0 +1,13 @@
+package seunghee._Do_it_자바_완전_정복._01_자바_시작하기._04_자바_프로그램의_기본_구조;
+
+public class _02_variable {
+    public static void main(String[] args) {
+        int a = 3;
+        String b = "화면";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(b + "출력");
+        System.out.println(a + b + "출력");
+    }
+}
